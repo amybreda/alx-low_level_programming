273cabfe65cabf14e325b508abaa1a3c6214be85
amybreda <breda.amy13@gmail.com>
2022-06-05 14:39:55 -0700

    updates

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int i, j;
+for (i = 48; i <= 57; i++)
+{
+for (j = i + 1; j <= 57; j++)
+{
+if (i != j)
+{
+putchar(i);
+putchar(j);
+}
+if ((i != 56) || (j != 57))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
