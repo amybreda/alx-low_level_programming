f29e1a80ebcf15220f8b0823b662cbe5b671684a
amybreda <breda.amy13@gmail.com>
2022-06-14 12:58:33 -0700

    updates

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - print a sentence in reverse
+ * @s: the pointer which links to string
+ * Return: reverse of string
+ */
+void print_rev(char *s)
+{
+int i = 0; 
+while (s[i])
+i++;
+while (i--)
+_putchar(s[i]);
+_putchar('\n');
+}
